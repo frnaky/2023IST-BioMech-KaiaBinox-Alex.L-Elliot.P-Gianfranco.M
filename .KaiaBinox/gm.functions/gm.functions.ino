9e9929e552353b16257db4c1de5d7faed0ea42eb
elliottp123 <elliott.pezzutti@education.nsw.gov.au>
2023-11-01 09:44:17 +1100

    US fix

--- .KaiaBinox/gm.functions/gm.functions.ino
+++ .KaiaBinox/gm.functions/gm.functions.ino
@@ -4,15 +4,14 @@
 
 
 Ultrasonic myUltrasonicSensor(8);
-LED_BLUE bar(2);
+
 
 
 void setup()
 {
  Serial.begin(9600);
 
- pinMode(myLed, OUTPUT);
- digitalWrite(myLed, 0);
+
 }
 void loop()
 {
@@ -22,5 +21,5 @@ void loop()
   Serial.print(RangeInCentimeters);//0~400cm
   Serial.println(" cm");
   delay(250);
-  bar.setLevel(2);
+
 }
