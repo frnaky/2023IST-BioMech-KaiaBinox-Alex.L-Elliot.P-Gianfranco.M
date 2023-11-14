72ac0f501042041d6b372142e03f3c5203493c33
elliottp123 <elliott.pezzutti@education.nsw.gov.au>
2023-11-14 11:04:20 +1100

    more work on finsalised version

--- .KaiaBinox/al.functions/al.functions.ino
+++ .KaiaBinox/al.functions/al.functions.ino
@@ -2,10 +2,15 @@
 #include <Wire.h>
 #include "Grove_LED_Matrix_Driver_HT16K33.h"
 
+#include "Ultrasonic.h"
+
+Ultrasonic myUSMIDDLE(8); // US VARS
+Ultrasonic myUSRIGHT(7);
+Ultrasonic myUSLEFT(4);
 
 // Create uint64_t type 8x8 matrix picture at
 // https://xantorohara.github.io/led-matrix-editor/#
-const uint64_t EXAMPLE[12] = {
+const uint64_t EXAMPLE[12] = { // idk why it has to be 12
     0xff07070707070707, // left
     0xffe0e0e0e0e0e0e0, // RIGHT BAR
     0x0707070707070707, // RIGHT BAR ( NOT DISPLAYING!!!)
@@ -27,17 +32,40 @@ void setup() {
     matrix.init();
     matrix.setBrightness(0);
     matrix.setBlinkRate(BLINK_OFF);
+    //dont touch^^
+
 }
 
 void loop() {
-    matrix.writeOnePicture(EXAMPLE[1]);
-    matrix.display();
-    delay(1000);
-    matrix.writeOnePicture(EXAMPLE[2]);
-    matrix.display();
-    delay(1000);
+  long MIDDLEUsRange;
+  long RIGHTUsRange;
+  long LEFTUsRange;
+  
+  MIDDLEUsRange = myUSMIDDLE.MeasureInCentimeters(); // **DIRECTION**US RANGE is now US READINGS MEASURED IN CENTIMETERS
+  RIGHTUsRange = myUSRIGHT.MeasureInCentimeters();
+  LEFTUsRange = myUSRIGHT.MeasureInCentimeters();
+
+Serial.print(MIDDLEUsRange);
+  if (MIDDLEUsRange <=5000){
+    Serial.print("poop");
 
 
-    //matrix.writePictures(EXAMPLE, 12, 1000, ACTION_SCROLLING);
-    //matrix.display();
+  }
+/*
+  MIDDLEUsRange = myUSMIDDLE.MeasureInCentimeters();
+  Serial.print("MIDDLE:");
+  Serial.print(MIDDLEUsRange);//0~400cm     // EXAMPLE CODE for US SENSOR middle lef and righr
+  Serial.println(" cm");
+  delay(250);
+  */
+  
+
+    /*matrix.writeOnePicture(EXAMPLE[1]); // left
+    matrix.display();
+    delay(1000);                              // test code
+    matrix.writeOnePicture(EXAMPLE[2]); // right
+    matrix.display();
+    delay(1000);
+    */
+
 }
